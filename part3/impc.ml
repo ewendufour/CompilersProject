64e4687001687732277da7dd20496b3131cdb00a
ewen575724 <ewen575724@gmail.com>
2024-10-28 02:09:54 +0100

    update part3

--- part3/impc.ml
+++ part3/impc.ml
@@ -1,4 +1,5 @@
 open Format
+open Lexing
 
 let usage = "usage: ./impc file.imp"
 
@@ -14,6 +15,12 @@ let file =
     Arg.parse spec set_file usage;
     match !file with Some f -> f | None -> Arg.usage spec usage; exit 1
 
+let report (b,e) =
+  let l = b.pos_lnum in
+  let fc = b.pos_cnum - b.pos_bol + 1 in
+  let lc = e.pos_cnum - b.pos_bol + 1 in
+  eprintf "File \"%s\", line %d, characters %d-%d:\n" file l fc lc
+
 let () =
   let cm = open_in "malloc.imp" in
   let lbm = Lexing.from_channel cm in
@@ -21,14 +28,23 @@ let () =
   close_in cm;
   let c  = open_in file in
   let lb = Lexing.from_channel c in
-  let prog = Impparser.program Implexer.token lb in
-  close_in c;
-  let prog = Imp.merge malloc prog in
-  let asm = Imp2mips.translate_program prog in
-  let output_file = (Filename.chop_suffix file ".imp") ^ ".asm" in
-  let out = open_out output_file in
-  let outf = formatter_of_out_channel out in
-  Mips.print_program outf asm;
-  pp_print_flush outf ();
-  close_out out;
-  exit 0
+  try
+    let prog = Impparser.program Implexer.token lb in
+    close_in c;
+    let prog = Imp.merge malloc prog in
+    let asm = Imp2mips.translate_program prog in
+    let output_file = (Filename.chop_suffix file ".imp") ^ ".asm" in
+    let out = open_out output_file in
+    let outf = formatter_of_out_channel out in
+    Mips.print_program outf asm;
+    pp_print_flush outf ();
+    close_out out;
+    exit 0
+  with
+  | Impparser.Error ->
+     report (lexeme_start_p lb, lexeme_end_p lb);
+     eprintf "syntax error@.";
+     exit 1
+  | e ->
+     eprintf "Anomaly: %s\n@." (Printexc.to_string e);
+     exit 2
--- part3/minimlc.ml
+++ part3/minimlc.ml
@@ -1,3 +1,6 @@
+open Format
+open Lexing
+
 let usage = "usage: ./minimlc file.ml"
 
 let spec = []
@@ -12,17 +15,33 @@ let file =
     Arg.parse spec set_file usage;
     match !file with Some f -> f | None -> Arg.usage spec usage; exit 1
 
+let report (b,e) =
+  let l = b.pos_lnum in
+  let fc = b.pos_cnum - b.pos_bol + 1 in
+  let lc = e.pos_cnum - b.pos_bol + 1 in
+  eprintf "File \"%s\", line %d, characters %d-%d:\n" file l fc lc
+
+
 let () =
   let c  = open_in file in
   let lb = Lexing.from_channel c in
-  let prog = Parser.program Lexer.token lb
-  in
-  close_in c;
-  let progclj = Mini2clj.translate_program prog in
-  let progimp = Clj2imp.translate_program progclj in
-  let output_file = (Filename.chop_suffix file ".ml") ^ ".imp" in
-  let out = open_out output_file in
-  Imppp.pp_program progimp out;
-  close_out out;
-  exit 0
-    
+  try
+    let prog = Parser.program Lexer.token lb
+    in
+    close_in c;
+    let progclj = Mini2clj.translate_program prog in
+    let progimp = Clj2imp.translate_program progclj in
+    let output_file = (Filename.chop_suffix file ".ml") ^ ".imp" in
+    let out = open_out output_file in
+    Imppp.pp_program progimp out;
+    close_out out;
+    exit 0
+  with
+  | Parser.Error ->
+     report (lexeme_start_p lb, lexeme_end_p lb);
+     eprintf "syntax error@.";
+     exit 1
+  | e ->
+     eprintf "Anomaly: %s\n@." (Printexc.to_string e);
+     exit 2
+
